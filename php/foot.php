fc867ed38b26ff789845ec60cb85742f5b784b93
Yeran Wicky <imagination5919@gmail.com>
2024-09-14 12:10:33 +0530

    jghhd

---
+++ php/foot.php
@@ -0,0 +1,10 @@
+<!-- JavaScript Libraries -->
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="lib/easing/easing.min.js"></script>
+<script src="lib/waypoints/waypoints.min.js"></script>
+<script src="lib/lightbox/js/lightbox.min.js"></script>
+<script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+<!-- Template Javascript -->
+<script src="js/main.js"></script>
--- php/head.php
+++ php/head.php
@@ -4,11 +4,22 @@
 <!-- Icon Font Stylesheet -->
 <link rel="stylesheet" href="css/all.min.css"/>
 <link href="css/bootstrap-icons.css" rel="stylesheet">
+
+<!-- Font awesome webfonts -->
+<link href="webfonts/fa-brands-400.eot" rel="stylesheet">
+<link href="webfonts/fa-brands-400.svg" rel="stylesheet">
 <link href="webfonts/fa-brands-400.ttf" rel="stylesheet">
+<link href="webfonts/fa-brands-400.woff" rel="stylesheet">
 <link href="webfonts/fa-brands-400.woff2" rel="stylesheet">
+<link href="webfonts/fa-regular-400.eot" rel="stylesheet">
+<link href="webfonts/fa-regular-400.svg" rel="stylesheet">
 <link href="webfonts/fa-regular-400.ttf" rel="stylesheet">
+<link href="webfonts/fa-regular-400.woff" rel="stylesheet">
 <link href="webfonts/fa-regular-400.woff2" rel="stylesheet">
+<link href="webfonts/fa-solid-900.eot" rel="stylesheet">
+<link href="webfonts/fa-solid-900.svg" rel="stylesheet">
 <link href="webfonts/fa-solid-900.ttf" rel="stylesheet">
+<link href="webfonts/fa-solid-900.woff" rel="stylesheet">
 <link href="webfonts/fa-solid-900.woff2" rel="stylesheet">
 <link href="webfonts/fa-v4compatibility.ttf" rel="stylesheet">
 <link href="webfonts/fa-v4compatibility.woff2" rel="stylesheet">
@@ -23,14 +34,3 @@
 
 <!-- Template Stylesheet -->
 <link href="css/style.css" rel="stylesheet">
-
-<!-- JavaScript Libraries -->
-<script src="js/jquery.min.js"></script>
-<script src="js/bootstrap.bundle.min.js"></script>
-<script src="lib/easing/easing.min.js"></script>
-<script src="lib/waypoints/waypoints.min.js"></script>
-<script src="lib/lightbox/js/lightbox.min.js"></script>
-<script src="lib/owlcarousel/owl.carousel.min.js"></script>
-
-<!-- Template Javascript -->
-<script src="js/main.js"></script>
